1e7f3f46e4ff825d55d51e2be4c46aa33a7ae7da
kdake40 <115654959+kdake40@users.noreply.github.com>
2024-02-11 10:26:50 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','campus') or die('connection failed');
+?>
---
+++ index.php
@@ -0,0 +1,88 @@
+<?php 
+    include 'core/init.php' ;
+    
+    if (isset($_SESSION['user_id'])) {
+      header('location: home.php');
+    }
+   
+?>
+
+<html>
+	<head>
+		<title>Campus</title>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css"/>
+        <!-- <link rel="stylesheet" href="assets/css/style-complete.css"/> -->
+        <link rel="stylesheet" href="assets/css/index_style.css?v=<?php echo time(); ?>">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/all.min.css">
+
+		<link rel="shortcut icon" type="image/png" href="assets/images/twitter.svg"> 
+	</head>
+<body>
+<main class="twt-main">
+            <section class="twt-login">
+                <?php include 'includes/login.php';  ?>
+                    <div class="slow-login">
+                        <!-- <img class="login-bird" src="twitter-logo.png" alt="bird"> -->
+                        <button class="login-small-display signin-btn pri-btn">Log in</button>
+                        <span class="front-para">See what’s happening in Our Campus</span>
+                        <span class="join">Campus</span>
+                        <button type="button" id="auto" onclick="" class="signup-btn pri-btn" data-toggle="modal" data-target="#exampleModalCenter">
+                            Register</button>
+                            
+                             
+                            <!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 style="font-weight: 700;" class="modal-title" id="exampleModalLongTitle">Register For Free</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+                    
+         <?php  include 'includes/signup-form.php' ?>
+      </div>
+      
+    </div>
+  </div>
+</div>
+                    </div>
+            </section>
+            <section class="twt-features">
+                <div class="features-div">
+                    <img class="twt-icon" src='https://image.ibb.co/bzvrkp/search_icon.png'>
+                    <p>Connect with like-minded people.</p>
+                    <img class="twt-icon" src="https://image.ibb.co/mZPTWU/heart_icon.png">
+                    <p>Stay updated on trending topics.</p>
+                    <img class="twt-icon" src="https://image.ibb.co/kw2Ad9/conv_icon.png">
+                    <p>Share your thoughts and opinions.</p>
+                </div>
+            </section>
+            <footer>
+                <ul>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Help Center</a></li>
+                    <li><a href="#">Terms</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Cookies</a></li>
+                    <li><a href="#">Ads info</a></li>
+                    <li><a href="#">Brand</a></li>
+                    <li><a href="#">Advertise</a></li>
+                    <li><a href="#">Developers</a></li>
+                    <li><a href="#">Settings</a></li>
+                    <li>© 2021 - Campus</li>
+                    <li style="color:#1DA1F2;"><b>- Developed by anonymous -</b></li>
+                </ul>
+            </footer>
+        </main>
+        
+        <script src="assets/js/jquery-3.4.1.slim.min.js"></script>
+        <script src="assets/js/popper.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/mine.js"></script>
+</body>
+</html>
